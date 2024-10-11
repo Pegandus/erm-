499ecc2dd6eae29c4186c256b30df5b0094e4e99
Pegandus <80130971+Pegandus@users.noreply.github.com>
2024-10-11 15:32:43 +0100

    Add files via upload

---
+++ IDLE online/client.py
@@ -0,0 +1,85 @@
+import pygame
+from network import Network
+
+width = 800
+height = 450
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Client")
+
+clientNumber = 0
+
+
+class Player():
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.rect = (x,y,width,height)
+        self.vel = 6
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, self.rect)
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_q]:
+            self.x -= self.vel
+
+        if keys[pygame.K_d]:
+            self.x += self.vel
+
+        if keys[pygame.K_z]:
+            self.y -= self.vel
+
+        if keys[pygame.K_s]:
+            self.y += self.vel
+
+        self.update()
+
+    def update(self):
+        self.rect = (self.x, self.y, self.width, self.height)
+
+
+def read_pos(str):
+    str = str.split(",")
+    return int(str[0]), int(str[1])
+
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+
+
+def redrawWindow(win,player, player2):
+    win.fill((0,0,0))
+    player.draw(win)
+    player2.draw(win)
+    pygame.display.update()
+
+
+def main():
+    run = True
+    n = Network()
+    startPos = read_pos(n.getPos())
+    p = Player(startPos[0],startPos[1],50,50,(0,0,255))
+    p2 = Player(0,0,50,50,(255,0,0))
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(60)
+        p2Pos = read_pos(n.send(make_pos((p.x, p.y))))
+        p2.x = p2Pos[0]
+        p2.y = p2Pos[1]
+        p2.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+
+        p.move()
+        redrawWindow(win, p, p2)
+
+main()
